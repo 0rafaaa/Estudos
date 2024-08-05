82d349728870ebb80ee281801331541bd170f7d1
0rafaaa <ra_caetano@hotmail.com>
2024-08-05 18:05:47 -0300

    novos exercicios

---
+++ Tipos-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    
+    public static void main(String[] args){
+        
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = numero *-1;
+
+        System.out.println(numero);
+
+        //x repeticao
+        
+
+    }
+}
---
+++ Tipos-variaveis/src/Testes.java
@@ -0,0 +1,15 @@
+public class Testes {
+    
+    public static void main (String[] args){
+
+        int meuSalario = 3000;
+
+        int meuAumento = 450;
+
+        int salarioTotal = meuSalario + meuAumento;
+
+        System.out.println("O VALOR TOTAL E: ");
+        System.out.println(salarioTotal);
+
+    }
+}
